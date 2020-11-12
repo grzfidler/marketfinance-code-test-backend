484adfb55be31f3bfa648ac56383eeba75b0cb3d
Grzegorz Fidler <grzfidler@gmail.com>
2020-11-12 22:32:12 +0100

    add service interface

---
+++ SlothEnterprise.ProductApplication/Interfaces/IProductApplicationService.cs
@@ -0,0 +1,9 @@
+﻿using SlothEnterprise.ProductApplication.DTOs.Applications;
+
+namespace SlothEnterprise.ProductApplication.Interfaces
+{
+    public interface IProductApplicationService
+    {
+        int SubmitApplicationFor(SellerApplicationDTO application)
+    }
+}
